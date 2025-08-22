4db77239c1bab853ee2a40b1e8f57c757a84c30b
TheDangle <edward.j.rangel@proton.me>
2025-08-22 13:51:21 +0200

    adding new game 'shooting gallery'

---
+++ shooting_gallery/main.lua
@@ -0,0 +1,38 @@
+-- declare global variables here. this block gets executed as soon as the game loads
+function love.load()
+	target = {} -- table we will refer too
+	target.x = 300
+	target.y = 300
+	target.radius = 50
+
+	score = 0
+	timer = 0
+
+	gameFont = love.graphics.newFont(40)
+end
+-- we can update and alter the variable in the update function
+function love.update(dt) -- dt stands for delta time
+end
+-- we then draw the variable on the screen
+function love.draw()
+	love.graphics.setColor(1, 0, 0)
+	love.graphics.circle("fill", target.x, target.y, target.radius)
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.setFont(gameFont)
+	love.graphics.print(score, 0, 0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if button == 1 then
+		local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+		if mouseToTarget < target.radius then
+			score = score + 1
+			target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+			target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+		end
+	end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+	return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+end
